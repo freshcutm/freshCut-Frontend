23be458150f7b54f9a9f9a7450bdd96e48bdb5e9
Gabonarvaezm <gabriel.narvaezm@campusuuc.edu.co>
2025-11-10 23:09:31 -0500

    nueva conexion

--- public/env.js
+++ public/env.js
@@ -3,4 +3,4 @@
 window.__env = window.__env || {};
 // Point to local backend default port 8080
 //window.__env.API_AI_URL = 'http://localhost:8080/api/ai';
-window.__env.API_AI_URL = 'https://freshcut-back.onrender.com';
+window.__env.API_AI_URL = 'https://freshcut-back.onrender.com/api/ai';
